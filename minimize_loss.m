b5d7bf9ea91de7ac618589042fb5525be3e3905d
Jacob Törngren <jacob.torngren@hotmail.com>
2023-07-31 22:00:36 +0200

    Added some comments

--- minimize_loss.m
+++ minimize_loss.m
@@ -1,7 +1,15 @@
+ % Monetary units to bet, this is the (equality) constraint
 bet = 1000;
-%odds = [3.5, 5.5, 6.5, 7, 9, 13, 15, 34, 67, 67, 101, 176];
-odds = [1.02 16 23];
 
+% Top 12 lowest odds from World Cup 2022
+%odds = [3.5, 5.5, 6.5, 7, 9, 13, 15, 34, 67, 67, 101, 176]; 
+
+% Same set of odds, but with the lowest odds omitted, enabling potential
+% profit for team betted on
+odds = [5.5, 6.5, 7, 9, 13, 15, 34, 67, 67, 101, 176];
+
+% Construct objective function with built-in constraint using nullspace
+% method
 n = length(odds);
 A = diag(odds-1)-(~eye(n,n));
 Atilde = [A, zeros(n,n)];
@@ -13,15 +21,19 @@ N = Q(:, (rank(B)+1):2*n);
 
 xB = bet/n*ones(1,2*n)';
 
+% Object or cost function to minimize, the maximum element of the loss vector
 objfunc = @(x) max(-Atilde*(xB+N*x),[],'all');
 
+% Initialize best winnings
 bestwins = -1000*ones(n,1);
 
+% Use fminsearch (Nelder-Mead) a couple of times with random initial
+% guesses to increase the search space.
 j = 0;
-for i = 1:10000
+for i = 1:1000
 
     guess = 400*rand(size(N,2),1)-200;
-    options = optimset('MaxFunEvals',1e14);
+    options = optimset('MaxFunEvals',1e16);
 
     [xN,FVAL,EXITFLAG, data] = fminsearch(objfunc,guess, options);
 
@@ -29,9 +41,10 @@ for i = 1:10000
     calcbet = round(xcalc(1:n),0);
     wins = round(A*calcbet,2);
     
+    % Save the best bet so far
     if min(wins) > min(bestwins)
         bestwins = wins;
-        bestbet = calcbet;
+        bestbets = calcbet;
         bestguess = xB + N*guess;
         bestguess = round(bestguess(1:n),0);
         fmindata = data;
@@ -39,4 +52,9 @@ for i = 1:10000
     end
     
 end
-bestbet
+disp("Optimized bets:")
+bestbets
+disp("Which would yield:")
+bestwins
+disp("If either those teams won")
+
